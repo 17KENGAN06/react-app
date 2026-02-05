23c0523264b8e14e558b2425de99903f254ff0d2
Mykyta Vlasov <kengangenkay@gmail.com>
2026-02-05 10:12:37 +0200

    import files to the main app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,27 @@
-import './app.js'
+import './app.css'
 
-import './app-filter/app-filter'
-import './app-info/app-info'
-import './employees-add-form/employees-add-form'
-import './employees-list/employees-list'
-import './employees-list-item/employees-list-item'
-import './search-panel/search-panel'
+import '../app-filter/app-filter'
+import '../app-info/app-info'
+import '../employees-add-form/employees-add-form'
+import '../employees-list/employees-list'
+import '../employees-list-item/employees-list-item'
+import '../search-panel/search-panel'
 
 const App = () => {
     return (
-      <div></div>  
-    )
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            
+            <EmployeesList/>
+            <EmployeesAddForm/>
+        </div>
+    
+    );
 }
 
 export default App;
