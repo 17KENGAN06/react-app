9fd99870a8a8ea48f5323d5ea0275874ec0e9888
Mykyta Vlasov <kengangenkay@gmail.com>
2026-02-05 10:09:11 +0200

    added app , deleted tests

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import './app.js'
+
+import './app-filter/app-filter'
+import './app-info/app-info'
+import './employees-add-form/employees-add-form'
+import './employees-list/employees-list'
+import './employees-list-item/employees-list-item'
+import './search-panel/search-panel'
+
+const App = () => {
+    return (
+      <div></div>  
+    )
+}
+
+export default App;
