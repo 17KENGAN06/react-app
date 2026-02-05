5c54ca52c6fc90fe2412115b61acac423b319a04
Mykyta Vlasov <kengangenkay@gmail.com>
2026-02-05 10:30:48 +0200

    changes 10:30

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,11 @@
 import './app.css'
 
-import '../app-filter/app-filter'
-import '../app-info/app-info'
-import '../employees-add-form/employees-add-form'
-import '../employees-list/employees-list'
-import '../employees-list-item/employees-list-item'
-import '../search-panel/search-panel'
+import AppFilter from '../app-filter/app-filter'
+import AppInfo from '../app-info/app-info'
+import EmployeesAddForm from'../employees-add-form/employees-add-form'
+import EmployeesList from '../employees-list/employees-list'
+import EmployeesListItem from '../employees-list-item/employees-list-item'
+import SearchPanel from '../search-panel/search-panel'
 
 const App = () => {
     return (
