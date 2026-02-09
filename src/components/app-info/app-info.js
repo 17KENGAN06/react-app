9e0a800164eb99a24a3e5c6d44e3d1af08a68788
KENGAN <kengangenkay@gmail.com>
2026-02-09 20:23:41 +0200

    UI-COP

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,13 +1,13 @@
-import './app-info.css'
+import './app-info.css';
 
 const AppInfo = () => {
-    return (
-        <div className="app-info">
-            <h1>Employee accounting in company N</h1>
-            <h2>Total number of employees:</h2>
-            <h2>The prize will be awarded to:</h2>
-        </div>
-    )
-}
+  return (
+    <div className="app-info">
+      <h1>Employee Overview</h1>
+      <h2>Total Employees:</h2>
+      <h2>Eligible for Bonus:</h2>
+    </div>
+  );
+};
 
 export default AppInfo;
