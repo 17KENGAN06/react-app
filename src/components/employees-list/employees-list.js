88dbda9b0314e4186aecd36b1868602a269719d7
Mykyta Vlasov <kengangenkay@gmail.com>
2026-02-11 13:57:05 +0200

    finished with statement

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,11 +2,11 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete }) => {
   const elements = data.map((item) => {
     const {id, ...itemProps} = item;
     return (
-      <EmployeesListItem onDelete={() => console.log('Deleted')} {...itemProps} key={id}/>
+      <EmployeesListItem onDelete={() => onDelete(id)} {...itemProps} key={id}/>
     )
   });
   return <ul className="app-list list-group">{elements}</ul>;
