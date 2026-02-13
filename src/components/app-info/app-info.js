00aed300f29b0ee9122874669f1de12a466e627b
KENGAN <kengangenkay@gmail.com>
2026-02-13 21:49:16 +0200

    test

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,9 +1,9 @@
 import './app-info.css';
 
-const AppInfo = ({onCounterIncrease, onCounterRise }) => {
+const AppInfo = ({ onCounterIncrease, onCounterRise }) => {
   return (
     <div className="app-info">
-      <h1>Employee Overview</h1>
+      <h1>Employee Overview </h1>
       <h2>Total Employees: {onCounterIncrease}</h2>
       <h2>Eligible for Bonus: {onCounterRise}</h2>
     </div>
